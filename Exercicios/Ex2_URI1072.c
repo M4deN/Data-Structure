ef1a2f5f68274e67c87b1f7ccdc18e65daf00144
Alécio L. Medeiros <43422425+AlexDeSaran@users.noreply.github.com>
2021-03-16 11:21:30 -0300

    Leia 2 valores inteiros X e Y. A seguir, calcule e mostre a soma dos números impares entre eles.  Entrada O arquivo de entrada contém dois valores inteiros.  Saída O programa deve imprimir um valor inteiro. Este valor é a soma dos valores ímpares que estão entre os valores fornecidos na entrada que deverá caber em um inteiro.
    Leia 2 valores inteiros X e Y. A seguir, calcule e mostre a soma dos números impares entre eles.
    
    Entrada
    O arquivo de entrada contém dois valores inteiros.
    
    Saída
    O programa deve imprimir um valor inteiro. Este valor é a soma dos valores ímpares que estão entre os valores fornecidos na entrada que deverá caber em um inteiro.

---
+++ Exercicios/Ex2_URI1072.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+ 
+int main(){
+ 
+  int x,y,soma,cont,temp;
+
+    while(scanf("%d%d",&x,&y)==2){
+        soma=0;
+
+        if(x>y){
+            temp=x;
+            x=y;
+            y=temp;
+        
+    }
+
+    if(x%2!=0){
+        for(cont=x+2;cont<y;cont++){
+            if(cont%2!=0)
+                soma+=cont;
+            }
+        }
+        else{
+            for(cont=x+1;cont<y;cont++){
+            if(cont%2!=0)
+                soma+=cont;
+            }
+        }
+        printf("%d\n",soma);
+    }
+ 
+    return 0;
+}
