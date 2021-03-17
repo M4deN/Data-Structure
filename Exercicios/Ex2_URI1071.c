97b904524c5c2268e12e718d6246853129b55b0d
Alécio L. Medeiros <43422425+AlexDeSaran@users.noreply.github.com>
2021-03-17 14:08:50 -0300

    Update Ex2_URI1071.c

--- Exercicios/Ex2_URI1071.c
+++ Exercicios/Ex2_URI1071.c
@@ -5,9 +5,11 @@ int main(){
   int x,y,soma,cont,temp;
 
     while(scanf("%d%d",&x,&y)==2){
+        
         soma=0;
 
         if(x>y){
+
             temp=x;
             x=y;
             y=temp;
@@ -15,14 +17,19 @@ int main(){
     }
 
     if(x%2!=0){
+
         for(cont=x+2;cont<y;cont++){
-            if(cont%2!=0)
+
+            if(cont%2!=0);
+
                 soma+=cont;
             }
         }
         else{
             for(cont=x+1;cont<y;cont++){
-            if(cont%2!=0)
+
+             if(cont%2!=0);
+
                 soma+=cont;
             }
         }
