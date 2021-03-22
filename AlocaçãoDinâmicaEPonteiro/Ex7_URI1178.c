9654d94cd38b5e32a51b53ee8baab8cacc78cdcb
Alécio L. Medeiros <43422425+AlexDeSaran@users.noreply.github.com>
2021-03-22 09:25:38 -0300

    Leia um valor X. Coloque este valor na primeira posição de um vetor N[100]. Em cada posição subsequente de N (1 até 99), coloque a metade do valor armazenado na posição anterior, conforme o exemplo abaixo. Imprima o vetor N.  Entrada A entrada contem um valor de dupla precisão com 4 casas decimais.  Saída Para cada posição do vetor N, escreva "N[i] = Y", onde i é a posição do vetor e Y é o valor armazenado naquela posição. Cada valor do vetor deve ser apresentado com 4 casas decimais.
    Leia um valor X. Coloque este valor na primeira posição de um vetor N[100]. Em cada posição subsequente de N (1 até 99), coloque a metade do valor armazenado na posição anterior, conforme o exemplo abaixo. Imprima o vetor N.
    
    Entrada
    A entrada contem um valor de dupla precisão com 4 casas decimais.
    
    Saída
    Para cada posição do vetor N, escreva "N[i] = Y", onde i é a posição do vetor e Y é o valor armazenado naquela posição. Cada valor do vetor deve ser apresentado com 4 casas decimais.

---
+++ AlocaçãoDinâmicaEPonteiro/Ex7_URI1178.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h> 
+
+int main(){
+	
+    int x;
+    double y;
+    double *n;
+
+    n = (double*) malloc(100*sizeof(double));
+    
+    scanf("%lf", &y);
+    
+    n[x]=y;
+    
+    for(x=0; x<100;x++){
+    	
+        printf("N[%d] = %.4lf\n", x, n[x]); 
+        y/=2;
+        n[x+1]=y;
+    }
+    
+    return 0;
+}
