cfd6f773560ef98476ad2b7afe35e7b38dee42f1
Alécio L. Medeiros <charlie_brandbury@outlook.com>
2021-03-31 17:51:12 -0300

    Update Ex15_URI1040.c

--- AlocaçãoDinâmicaEPonteiro/Ex15_URI1040.c
+++ AlocaçãoDinâmicaEPonteiro/Ex15_URI1040.c
@@ -15,18 +15,27 @@ int main(){
 	alunos = (Aluno*) malloc(sizeof(Aluno));
 	
     scanf ("%lf%lf%lf%lf", &alunos->nota1,&alunos->nota2,&alunos->nota3,&alunos->nota4);
+
     alunos->v1=((alunos->nota1*2)+(alunos->nota2*3)+(alunos->nota3*4)+alunos->nota4)/10;
+
     printf ("Media: %.1lf\n",alunos->v1);
+
     if(alunos->v1>=7)
+
         printf ("Aluno aprovado.\n");
+
     else if(alunos->v1<5)
+
         printf("Aluno reprovado.\n");
+
     else if(alunos->v1>=5&&alunos->v1<=6.9)
     {
         printf ("Aluno em exame.\n");
         scanf("%lf", &alunos->nota5);
         printf ("Nota do exame: %.1lf\n",alunos->nota5);
+
         alunos->v2=(alunos->nota5+alunos->v1)/2;
+        
         if(alunos->v2>=5)
             printf ("Aluno aprovado.\n");
         else if(alunos->v2<=4.9)
