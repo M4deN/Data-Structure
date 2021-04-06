6a88fd58b6a658555446b078d920167482e91ccf
Alécio L. Medeiros <charlie_brandbury@outlook.com>
2021-04-06 18:17:04 -0300

    Leia 3 valores de ponto flutuante e efetue o cálculo das raízes da equação de Bhaskara. Se não for possível calcular as raízes, mostre a mensagem correspondente “Impossivel calcular”, caso haja uma divisão por 0 ou raiz de numero negativo.  Entrada Leia três valores de ponto flutuante (double) A, B e C.  Saída Se não houver possibilidade de calcular as raízes, apresente a mensagem "Impossivel calcular". Caso contrário, imprima o resultado das raízes com 5 dígitos após o ponto, com uma mensagem correspondente conforme exemplo abaixo. Imprima sempre o final de linha após cada mensagem.
    Leia 3 valores de ponto flutuante e efetue o cálculo das raízes da equação de Bhaskara. Se não for possível calcular as raízes, mostre a mensagem correspondente “Impossivel calcular”, caso haja uma divisão por 0 ou raiz de numero negativo.
    
    Entrada
    Leia três valores de ponto flutuante (double) A, B e C.
    
    Saída
    Se não houver possibilidade de calcular as raízes, apresente a mensagem "Impossivel calcular". Caso contrário, imprima o resultado das raízes com 5 dígitos após o ponto, com uma mensagem correspondente conforme exemplo abaixo. Imprima sempre o final de linha após cada mensagem.

---
+++ AlocaçãoDinâmicaEPonteiro/Fórmula_De_Bhaskara.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+typedef struct bhaskara{
+	
+	double val1,val2,val3,val4,val5,val6,val7;
+
+}Bhaskara;
+
+int main(){
+	
+	Bhaskara bhaskara, *bhask;
+	bhask = &bhaskara;
+	bhask = (Bhaskara*) malloc(sizeof(Bhaskara));
+	
+   
+    scanf ("%lf%lf%lf", &bhask->val1,&bhask->val2,&bhask->val3);
+    bhask->val4=bhask->val2*bhask->val2-4*bhask->val1*bhask->val3;
+    bhask->val5=pow(bhask->val4,.5);
+    if(bhask->val4<0||bhask->val1==0)
+        printf ("Impossivel calcular\n");
+
+    else{
+
+        bhask->val6=(-bhask->val2+bhask->val5)/(2*bhask->val1);
+        bhask->val7=(-bhask->val2-bhask->val5)/(2*bhask->val1);
+        printf ("R1 = %.5lf\n",bhask->val6);
+        printf ("R2 = %.5lf\n",bhask->val7);
+
+    }
+    
+    return 0;
+}
