d61e918213b48d5c37a5ebf963b3f1dca4f25c4c
Alécio L. Medeiros <charlie_brandbury@outlook.com>
2021-04-10 21:57:01 -0300

    Entrada A primeira linha de entrada contem um número L (0 ≤ L ≤ 11) indicando a linha que será considerada para operação. A segunda linha de entrada contém um único caractere Maiúsculo T ('S' ou 'M'), indicando a operação (Soma ou Média) que deverá ser realizada com os elementos da matriz. Seguem os 144 valores de ponto flutuante que compõem a matriz, sendo que a mesma é preenchida linha por linha, da linha 0 até a linha 11, sempre da esquerda para a direita.  Saída Imprima o resultado solicitado (a soma ou média), com 1 casa após o ponto decimal.
    Entrada
    A primeira linha de entrada contem um número L (0 ≤ L ≤ 11) indicando a linha que será considerada para operação. A segunda linha de entrada contém um único caractere Maiúsculo T ('S' ou 'M'), indicando a operação (Soma ou Média) que deverá ser realizada com os elementos da matriz. Seguem os 144 valores de ponto flutuante que compõem a matriz, sendo que a mesma é preenchida linha por linha, da linha 0 até a linha 11, sempre da esquerda para a direita.
    
    Saída
    Imprima o resultado solicitado (a soma ou média), com 1 casa após o ponto decimal.

--- Exercicios/Linha_Da_Matriz.c
+++ Exercicios/Linha_Da_Matriz.c
@@ -9,21 +9,28 @@ int main(){
 
     scanf("%d", &C);
     scanf("%s", &T);
+    
     for(x=0;x<=11;x++){
 
         for(y=0; y<=11; y++){
-            
+
         scanf("%lf", &mat[x][y]);
+
         if(x==C)
+
             a+=mat[x][y];
         }
     }
     if(T[0]=='S')
+
         printf("%.1lf\n",a);
+
     else if(T[0]=='M')
     {
         a=a/12.0;
+
         printf("%.1lf\n",a);
     }
-    return 0;
+
+    return 1;
 }
