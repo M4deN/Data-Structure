8cd9585d38a0f9226b3f3afcd1fe2593354b5c03
Alécio L. Medeiros <charlie_brandbury@outlook.com>
2021-05-02 09:30:08 -0300

    Update Fila.c

--- Lista_Pilha_Fila/Fila.c
+++ Lista_Pilha_Fila/Fila.c
@@ -32,29 +32,36 @@ int main() {
         }
 
         printf("Cartas descartadas: ");
-        
+
         for(i=1; i < num; i++) {
+
             printf("%d, ", dequeue(&q));
+
             enqueue(&q, dequeue(&q));
         }
 
         printf("\nCarta restante: %d\n", dequeue(&q));
+
         }
 
-        void newQueue(queue *q) {
-        q->head = 0;
-        q->tail = 0;
+        void newQueue(queue *q){
+
+            q->head = 0;
+            q->tail = 0;
         }
 
-        int isEmpty(queue *q) {
+        int isEmpty(queue *q){
+
         return q->head == q->tail;
+
         }
 
-        int isFull(queue *q) {
-        return q->tail == N;
+        int isFull(queue *q){
+
+            return q->tail == N;
         }
 
-        void enqueue(queue *q, int num) {
+        void enqueue(queue *q, int num){
         if (isFull(q)) {
             printf("Queue overflow!\n");
             exit(1);
