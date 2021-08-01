7c12dfb674ace4a53d10a386e1ba2c961045f465
Alécio L. Medeiros <charlie_brandbury@outlook.com>
2021-08-01 11:46:32 -0300

    Update Movimento.c

--- Lista_Pilha_Fila/Movimento.c
+++ Lista_Pilha_Fila/Movimento.c
@@ -92,8 +92,9 @@ void mergeQueues(queue *target, queue *q1, queue *q2) {
   if (size(q1) > size(q2)) {
 
     clone(q1, &maior);
-    
+
     clone(q2, &menor);
+    
   } else {
     clone(q1, &menor);
     clone(q2, &maior);
